eeac8cfe5ec4e98cb4735a6ee060009ea67fa5a9
brafios <brafios8@gmail.com>
2025-05-08 09:37:56 -0300

    aprendendo react

---
+++ meu-projeto/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Conteudo from './conteudo/conteudo'
+
+function App() {
+  
+
+  return (
+    <div>
+      
+      <header>
+        <h1>Header</h1>
+      </header>
+
+      <Conteudo/>
+
+      <footer>
+        <h1>footer</h1>
+      </footer>
+
+    </div>
+  )
+}
+
+export default App
---
+++ meu-projeto/src/conteudo/conteudo.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Conteudo(){
+
+    return(
+
+        <>
+            <h1>Meu Conteudo</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus id, necessitatibus modi illum eos saepe praesentium quod corporis similique libero cupiditate reprehenderit distinctio quibusdam incidunt quasi mollitia veritatis quisquam eaque!</p>
+        </>
+
+    )
+
+}
